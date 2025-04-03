7ab2a564e65ec648c67e10278bf57f2efa18d244
homepai26 <homepai26@gmail.com>
2025-04-03 08:49:36 +0000

    update database value

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'concert',
-    password: '1234',
+    user: 'concert_user',
+    password: '11037',
     database: 'concert_booking',
     waitForConnections: true,
     connectionLimit: 10,
