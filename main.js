2079098a0ff19059a10169c6e8a5b451dc630f4e
homepai26 <homepai26@gmail.com>
2025-04-03 22:49:47 +0700

    first hand writing

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-express = require('express');
-const app = express();
+const express = require('express');
+const mysql = require('mysql2/promise');
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+const app = express();
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'concert_user',
+  database: 'concert_booking',
+  waitForConnections: true,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
 
-app.listen(3000);
+const main = async() => {
+    try {
+	const [row, field] = pool.execute('SELECT * FROM users');
+	app.get('/', (req, res) => {
+	    res.send(JSON.stringify(field));
+	});
+
+	app.listen(3000);
+    } catch (error) {
+	if (error)
+	    console.log(error);
+    }
+};
+
+main();
