23e5e6549b218d088440556c5efd2fee4580706c
homepai26 <homepai26@gmail.com>
2025-04-05 00:09:25 +0700

    now funstion sequence so now it working normally

--- route.js
+++ route.js
@@ -4,36 +4,30 @@ const pool = require('./config/database');
 const sql = require('./sql');
 
 router.post('/api/add_concert', async(req, res) => {
-    var obj_arr = [];
-    var count = 0;
-    await pool.execute('START TRANSACTION');
-    let result1 = await sql.add_concert_info(req.body.concert_name, req.body.concert_artist,
-					 req.body.concert_venue, req.body.concert_timeshow);
-    obj_arr[count] = result1; 
-    count++;
-    
-    req.body.seats.forEach(async(value) => {
-	let result2 = await sql.add_seat_type(value.type, value.price);
-	let result3 = await sql.add_concert_seat(result1.insertId, result2.insertId, value.seat_start, value.seat_end, value.seat_available);
-	obj_arr[count] = result2;
-	obj_arr[count + 1] = result3;
-	count += 2;
-    });
-
-    if (obj_arr) {
-	pool.execute('COMMIT');
-	console.log("commit");
-	res.json(obj_arr);
-    } else {
-	pool.execute('ROLLBACK');
-	console.log("rollback");
-	res.json(obj_arr);
+    try {
+	await pool.execute('START TRANSACTION');
+	result1 = await sql.add_concert_info(req.body.concert_name, req.body.concert_artist,
+					     req.body.concert_venue, req.body.concert_timeshow);
+	
+	await req.body.seats.forEach(async(value) => {
+	    result2 = await sql.add_seat_type(value.type, value.price);
+	    result3 = await sql.add_concert_seat(result1.insertId, result2.insertId, value.seat_start, value.seat_end, value.seat_available);
+	});
+	await pool.execute('COMMIT');
+	res.send("added successfully~");
+    } catch (error) {
+	await pool.execute('ROLLBACK');
+	res.send(error);
     }
 });
 
 router.get('/api/concert', async(req, res) => {
-    result = await sql.view_concert();
-    res.json(result);
+    try {
+	result = await sql.view_concert();
+	res.json(result);
+    } catch (error) {
+	console.log(error);
+    }
 });
 
 module.exports = router;
--- sql.js
+++ sql.js
@@ -3,84 +3,56 @@ var result = null;
 
 const add_concert_info = async(concert_name, concert_artist, concert_venue, concert_timeshow) => {
     if (concert_name && concert_artist && concert_venue && concert_timeshow) {
-	try {
-	    [result] = await pool.execute('INSERT INTO concert_info(concert_name, concert_artist, concert_venue, concert_timeshow) VALUES (?,?,?,?)',
-					  [concert_name, concert_artist, concert_venue, concert_timeshow]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO concert_info(concert_name, concert_artist, concert_venue, concert_timeshow) VALUES (?,?,?,?)',
+				      [concert_name, concert_artist, concert_venue, concert_timeshow]);
     }
     return result;
 };
 
 const add_concert_seat = async(concert_id, seat_type_id, seat_start, seat_end, seat_available) => {
     if (concert_id && seat_type_id && seat_start && seat_end && seat_available) {
-	try {
-	    [result] = await pool.execute('INSERT INTO concert_seat VALUES (?,?,?,?,?)',
-					  [concert_id, seat_type_id, seat_start, seat_end, seat_available]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO concert_seat VALUES (?,?,?,?,?)',
+				      [concert_id, seat_type_id, seat_start, seat_end, seat_available]);
     }
     return result;
 };
 
 const add_thai_customer = async(name, birthdate, email, id) => {
     if (name && birthdate && email && id) {
-	try {
-	    [result] = await pool.execute('INSERT INTO customer(name, birthdate, email, id) VALUES (?,?,?,?)',
-					  [name, birthdate, email, id]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO customer(name, birthdate, email, id) VALUES (?,?,?,?)',
+				      [name, birthdate, email, id]);
     }
     return result;
 };
 
 const add_foreign_customer = async(name, birthdate, email, passport) => {
     if (name && birthdate && email && id) {
-	try {
-	    [result] = await pool.execute('INSERT INTO customer(name, birthdate, email, passport) VALUES (?,?,?,?)',
-					  [name, birthdate, email, passport]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO customer(name, birthdate, email, passport) VALUES (?,?,?,?)',
+				      [name, birthdate, email, passport]);
     }
     return result;
 };
 
 const add_reserved_seat = async(concert_id, seat_no, customer_id) => {
     if (concert_id && seat_no && customer_id) {
-	try {
-	    [result] = await pool.execute('INSERT INTO reserved_seat VALUES (?,?,?)',
-					  [concert_id, seat_no, customer_id]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO reserved_seat VALUES (?,?,?)',
+				      [concert_id, seat_no, customer_id]);
     }
     return result;
 };
 
 const add_seat_type = async(type, price) => {
     if (type && price) {
-	try {
-	    [result] = await pool.execute('INSERT INTO seat_type(type, price) VALUES (?,?)',
-					  [type, price]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO seat_type(type, price) VALUES (?,?)',
+				      [type, price]);
     }
     return result;
 };
 
 const add_ticket = async(concert_name, name, venue, seat, timeshow, purchase_datetime) => {
     if (concert_name && name && venue && seat && timeshow && purchase_datetime) {
-	try {
-	    [result] = await pool.execute('INSERT INTO ticket(concert_name, name, venue, seat, timeshow, purchase_datetime) VALUES (?,?,?,?,?,?)',
-					  [concert_name, name, venue, seat, timeshow, purchase_datetime]);
-	} catch (error) {
-	    console.log(error);
-	}
+	[result] = await pool.execute('INSERT INTO ticket(concert_name, name, venue, seat, timeshow, purchase_datetime) VALUES (?,?,?,?,?,?)',
+				      [concert_name, name, venue, seat, timeshow, purchase_datetime]);
     }
     return result;
 };
