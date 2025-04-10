fe858cf1f10a3f4514c203e0e7bc100a23913c9c
homepai26 <homepai26@gmail.com>
2025-04-10 11:37:08 +0700

    remind myself to not forget to change password

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'warit.pp.ua',
     user: 'concert_user',
+    //password will change before public, so don't worry
     password: 'OPyyMk99f67Z',
     database: 'concert_booking',
     waitForConnections: true,
